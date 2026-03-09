143786f9bbd433c8110997f9ffd815e2fe08ea34
dfamrln <mhdffa1@gmail.com>
2026-03-09 11:09:00 +0700

    Menambahkan input nilai Bahasa dan Matematika

--- nested_if.cpp
+++ nested_if.cpp
@@ -19,4 +19,10 @@ int main()
     float nilBI, nilMT, rerata;
     string status;
 
+    cout << "Masukkan nilai Bahasa Inggris = ";
+    cin >> nilBI;
+    cout << "Masukkan nilai Matematika = ";
+    cin >> nilMT;
+
+
 }    
